02ecc350a6f50e497e0f3cb7a98acc46d188ee8d
manpreetkaur292006-design <manpreetkaur292006@gmail.com>
2026-06-06 16:32:02 +0530

    added all the changes made in the home page and navbar

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,41 @@
 import React from 'react'
 import './Hero.css'
+import profile from '../assets/profile.jpeg'
 
 const Hero = () => {
   return (
     <section id='hero'>
-      <h1>Hi, I'm Manpreet Kaur</h1>
-      <h2>Frontend Developer</h2>
+      <div className="container hero-container">
+        <div className="hero-content">
+          <p className='hero-greeting'>Hello, I'm</p>
+
+          <h1 className='hero-name'>Manpreet Kaur</h1>
+
+          <h2 className="hero-role">
+            Frontend Developer | React Developer | Computer Science Student
+          </h2>
+
+          <p className="hero-description">
+            I build responsive web application using React, JavaScript, 
+            HTML, and CSS while continuously learning modern technologies
+            and creating meaningful digital experience. 
+          </p>
+
+          <div className="hero-buttons">
+            <a href="#projects" className="primary-btn">
+              View Projects
+            </a>
+
+            <a href="/Manpreet_Kaur.pdf" download className="secondary-btn">
+              Download Resume
+            </a>
+          </div>
+        </div>
+
+        <div className="hero-image">
+            <img src={profile} alt="Manpreet Kaur" />
+        </div>
+      </div>
     </section>
   )
 }
