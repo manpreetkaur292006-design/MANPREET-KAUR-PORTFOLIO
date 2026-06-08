78a6972e267f72b7e98129d1e65d726887769c58
manpreetkaur292006-design <manpreetkaur292006@gmail.com>
2026-06-08 18:18:56 +0530

    made some changes in the jsx file of the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,32 +1,35 @@
-import React from 'react'
-import './Navbar.css'
+import React from "react";
+import "./Navbar.css";
 
 const Navbar = () => {
   return (
-    <nav id='navbar'>
-      <div className='container navbar-container'>
+    <nav id="navbar">
+      <div className="container navbar-container">
 
-        <a href="#hero" className='logo'>
+        <a href="#hero" className="logo">
           MK
         </a>
 
-        <ul className='nav-links'>
+        <ul className="nav-links">
           <li><a href="#about">About</a></li>
           <li><a href="#skills">Skills</a></li>
           <li><a href="#projects">Projects</a></li>
+          <li><a href="#achievements">Achievements</a></li>
           <li><a href="#contact">Contact</a></li>
         </ul>
 
         <a
-          href='/Manpreet_Kaur.pdf'
-          className='resume-btn'
+          href="/Manpreet_Kaur.pdf"
+          target="_blank"
+          rel="noreferrer"
+          className="resume-btn"
         >
-          View Resume
+          Resume
         </a>
 
       </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
